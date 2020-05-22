ed247c98e055d8c8c7c806afc36e1643fc950d6a
liuyujiangx <1914571065@qq.com>
2020-05-22 12:15:28 +0800

    upload

--- app/routes/views.py
+++ app/routes/views.py
@@ -18,7 +18,8 @@ from . import home
 # 修改文件名称
 def change_filename(filename):
     fileinfo = os.path.splitext(filename)
-    filename = datetime.datetime.now().strftime("%Y%m%d%H%%M") + str(uuid.uuid4().hex) + fileinfo[-1]
+    filename = datetime.datetime.now().strftime("%Y%m%d%H%M%S") + str(uuid.uuid4().hex) + fileinfo[-1]
+    print(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
     return filename
 
 
