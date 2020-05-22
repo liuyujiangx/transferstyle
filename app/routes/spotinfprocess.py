69a31c34437aff3daa26a58789b1b26d8309ce74
liuyujiangx <1914571065@qq.com>
2020-05-22 10:14:43 +0800

    upload

--- app/routes/spotinfprocess.py
+++ app/routes/spotinfprocess.py
@@ -1,7 +1,7 @@
 import docx
 def opens():
     dp = []
-    val = r'{}'.format('C:\\Users\\19145\\Desktop\\学习\\实训\\风格迁移\\景点.docx')
+    val = r'{}'.format()
     open = docx.Document(val)
     for t in open.paragraphs:
         dp.append(t.text)
